7f27a1a349db8d96bd30cc76d8a0ea463dffe077
Brandon Sorgdrager <brandon.sorgdrager@gmail.com>
2020-10-20 18:22:26 -0500

    Update driver.go

--- backends/storage/redigo/driver.go
+++ backends/storage/redigo/driver.go
@@ -5,6 +5,6 @@ import redigo "github.com/gomodule/redigo/redis"
 
 func init() {
 	backends.RedisDialer = func(network, address string, options ...backends.RedisDialOption) (backends.RedisConn, error) {
-		return redigo.Dial(network, address)
+		return redigo.DialURL(address)
 	}
 }
